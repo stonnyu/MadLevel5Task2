372d82c015d86b8a5ae8ae11686541fcba768629
Tonny <mrtonnysu@gmail.com>
2021-01-02 17:58:03 +0100

    Forgot to add these files in commits

---
+++ app/src/main/java/com/example/madlevel5task2/ui/GameAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.madlevel5task2.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel5task2.R
+import com.example.madlevel5task1.model.Game
+import kotlinx.android.synthetic.main.item_game.view.*
+import java.text.SimpleDateFormat
+
+class GameAdapter(private val games: List<Game>) : RecyclerView.Adapter<GameAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun databind(game: Game) {
+            val format = SimpleDateFormat("dd MMMM yyyy")
+
+            itemView.tvBacklogGameTitle.text = game.title
+            itemView.tvBacklogGamePlatform.text = game.platform
+            itemView.tvBacklogGameDate.text = String.format("Release: %s", format.format(game.date))
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_game, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return games.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(games[position])
+    }
+
+}
